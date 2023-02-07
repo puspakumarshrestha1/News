f5e757a0316afdb8d93704ca5fac7731737be746
sachin1144 <83352901+sachin1144@users.noreply.github.com>
2023-02-07 23:39:07 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Footer from './Components/Footer'
 import Header from './Components/Header'
 import "./css/style.css"
 import "./css/responsive.css"
+import Home from './Components/Home'
 function App() {
   return (
     <>
       <Header/>
+      <Home/>
       <Footer/>
     </>
   )
